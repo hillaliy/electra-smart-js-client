a203a3046687422e15a667519dfae7a0e2239cf5
hillaliy <hillaliy@me.com>
2026-03-14 08:30:05 +0200

    chore (deps): update devDependencies

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w*)\s*(?:\((.*)\))?: (.*)$/,
+    },
+  },
   rules: {
-    'subject-case': [1, 'always', ['sentence-case']],
+    'subject-case': [1, 'always', ['lower-case']],
     'header-max-length': [1, 'always', 72],
   },
 };
